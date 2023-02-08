3dc392cd2289c10b1a62b2b8cdf89a3bceb393ee
Replit user <>
2023-02-08 15:10:34 +0000

    feat: implement  even/odd devision

--- tasks/even_odd.py
+++ tasks/even_odd.py
@@ -9,4 +9,17 @@ def even_odd(arr: list[int]) -> float:
     Пример:
     even_odd([1, 2, 3, 4, 5]) == 0.8889
     """
-    raise NotImplementedErro
+    even_sum = 0.0
+    odd_sum = 0.0
+
+    for i in range(len(arr)):
+      if arr[i]%2 == 0:
+        even_sum += arr[i]
+      else:
+        odd_sum += arr[i]
+    if odd_sum != 0.0:
+      output = even_sum/odd_sum
+    else:
+      output = 0
+     
+    return output
