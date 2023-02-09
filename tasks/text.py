276b75853d13259023006a5984853321b4f2991c
mrFZ <zkobin.f@gmail.com>
2023-02-09 14:38:36 +0000

    feat: add short and long word finder from string

--- tasks/text.py
+++ tasks/text.py
@@ -10,5 +10,11 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    output = (None,None)
+    text = text.strip().replace(',','')
+    text_list = text.split()
+    if len(text) == 0:
+      output = (None,None)
+    else:
+      word_len = [len(word) for word in text_list]
+      output = (text_list[word_len.index(min(word_len))],text_list[word_len.index(max(word_len))])
     return output
