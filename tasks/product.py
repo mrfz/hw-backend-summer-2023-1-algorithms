8cf3b562347fcec7ce97aad259f584df5057db0b
mrFZ <zlobin.f@gmail.com>
2023-02-12 04:52:25 +0000

    feat: implement coretesian product function

--- tasks/product.py
+++ tasks/product.py
@@ -11,4 +11,7 @@ def cartesian_product(arr1: list[Any], arr2: list[Any]) -> list:
     cartesian_product([1, 2], [3, 4]) == [(1, 3), (1, 4), (2, 3), (2, 4)]
     """
     output = []
+    for i in range (len(arr1)):
+      for j in range(len(arr2)):
+        output.append((arr1[i], arr2[j]))
     return output
